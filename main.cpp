fad9d17b28f8d5c0dfc80ddb0e47f1f08335f332
Stobee <113770557+Stobee@users.noreply.github.com>
2026-03-11 17:05:47 +0900

    동적 배열 출력

--- main.cpp
+++ main.cpp
@@ -5,21 +5,41 @@ using namespace std;
 
 
 
+void ShowFlexArray(int Num) {
+	
+	int* NumArray = new int[Num];
 
+	for (int i = 0; i < Num; i++)
+	{
+		NumArray[i] = i + 1;
+		cout << NumArray[i] << " ";
+	}
+	delete[] NumArray;
+				
+}
 
 int main()
 {
+	// 숫자를 입력 받아서 그걸 크기로 숫자 배열을 만듬
+	// 1부터 크기까지 초기화 하고 출력하는 프로그램
 
-	char TextArray[6] = { 'A', 'P', 'P', 'L', 'E','\0' };
-	int Count = 0; // 문자열 개수
-	
-	Count = GetCharArrayLength(TextArray, 7);
-	
-	cout <<"총 " << Count << " 개" << endl;
-	Replace(TextArray, 7, 'P', 'B');
-	
-	WhereIsCharInArray(TextArray,'L',7);
+	while (true)
+	{	
+		int Num = 0;
+		cin >> Num;
+		if (Num < 1000)
+		{
+			ShowFlexArray(Num);
+			break;
+		}
+		else
+		{
+			cout << "1000보다 작은 수를 입력해주세요" << endl;
+		}
+	}
 
+	
+	
 
 	return 0;
 }
